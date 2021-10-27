22dfec7220eec9aac1df90233d7daa78c17bdfb1
Bogdan <bgra4ov@gmail.com>
2021-10-27 12:44:20 +0300

    mini carousel is under development

--- js/carousel.js
+++ js/carousel.js
@@ -1,10 +1,10 @@
 (function () {
     const product = JSON.parse(localStorage.product);
     const slides = [
-        `<img src="${product.image}" alt="white chair">`,
-        `<img src="./img/best__selling-products/pink-chair.png" alt="white chair">`,
-        `<img src="./img/best__selling-products/green-chair.png" alt="white chair">`,
-        `<img src="./img/best__selling-products/white-chair.png" alt="white chair">`,
+        `<img class="demo" src="${product.image}" alt="white chair">`,
+        `<img class="demo" src="./img/best__selling-products/pink-chair.png" alt="white chair">`,
+        `<img class="demo" src="./img/best__selling-products/green-chair.png" alt="white chair">`,
+        `<img class="demo" src="./img/best__selling-products/white-chair.png" alt="white chair">`,
     ];
 
     let currentSlide = 0;
@@ -13,12 +13,30 @@
         const slideContainer = document.querySelector(".container-carousel .slide");
         slideContainer.innerHTML = slides[currentSlide];
     }
-    function showMiniCarousel() {
+    /*function showMiniCarousel() {
         const rowContainer = document.querySelector(".container-carousel-row .slide-rows");
         for (let i = 0; i < slides.length; i++) {
             rowContainer.innerHTML += `<img ${currentSlide === i ? 'class="active"' : " "} src="${product.image}" alt="white chair">`;
     }
+    }*/
+    function showMiniCarousel(){
+        const rowContainer = document.querySelector(".container-carousel-row .slide-rows");
+        for (let i = 0; i < slides.length; i++) {
+            rowContainer.innerHTML += `<img ${'class="demo"'} src="${product.image}" alt="white chair">`;
+        }
+            const demo = document.querySelectorAll(".slide-rows .demo");
+            demo[currentSlide].classList.add("active");
+            for(let j = 0; j < demo.length; j++){
+            document.querySelector(".next").addEventListener('click', function(){
+                demo[currentSlide].classList.remove("active");
+                demo[currentSlide + 1].classList.add("active");
+                if (currentSlide >= demo.length) currentSlide = 0;
+            });
+
     }
+    }
+
+
 
     function nextSlide() {
         currentSlide++;
